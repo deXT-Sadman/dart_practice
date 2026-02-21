7c067306ad1555e95e846fc16d9d5c4ed7f69406
Sadman <2258cse00945@manarat.ac.bd>
2026-02-21 19:51:06 +0600

    Add initial implementation of list operations with ages and fruits

---
+++ bin/list2.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<int> ages = [23, 45, 12, 67, 34];
+  print(ages);
+
+  List<String> fruits = ['Apple', 'Banana', 'Orange', 'Grapes'];
+  print(fruits);
+
+  print(ages.length);
+  print(fruits.first);
+  print(fruits.last);
+  print(ages.reversed);
+  print(fruits.isEmpty);
+  print(ages.isNotEmpty);
+}
